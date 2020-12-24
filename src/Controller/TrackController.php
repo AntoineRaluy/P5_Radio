cb76598605573cfb30880a2ae08b0e0959effac2
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2020-12-24 10:28:46 +0100

    Search for existing track

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -31,57 +31,34 @@ class TrackController extends AbstractController
      * @Route("/tracks/new", name="app_track_new")
      */
     public function newtrack(Request $request, EntityManagerInterface $entityManager, TrackRepository $repoTrack)
-    {
+    {   
         $form = $this->createForm(TrackFormType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted()) {
             $track = $form->getData();
-            $entityManager->persist($track);
-            $entityManager->flush();
+            $title = $track->getTitle();
+            $artist = $track->getArtist();
 
-            return $this->redirectToRoute('app_tracklist');
-        }
-    
-        $searchForm = $this->createForm(SearchType::class);
-        $searchForm->handleRequest($request);
-        
-        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-            $trackSearch = $searchForm->getData();
-            $resultTrack = $repoTrack->findTrack($trackSearch);
+            $resultTrack = $repoTrack->findTrack($artist, $title);
+            
+            if ($resultTrack == !null) {
+                $this->addFlash('erreur', 'Le morceau est déjà dans la radio');
+                return $this->redirectToRoute('app_tracklist');
+                }
 
-            if ($resultTrack == null) {
-                $this->addFlash('erreur', 'Aucun morceau ne correspond dans la radio.');
+            else {
+
+                $entityManager->persist($track);
+                $entityManager->flush();
+                $this->addFlash('info', 'Le morceau est maintenant en attente de validation');
+                return $this->redirectToRoute('app_tracklist');
             }
-        }
-
+        } 
         return $this->render('track/newtrack.html.twig', [
             'trackForm' => $form->createView(),
-            'searchForm' => $searchForm->createView(),
-        ]);
-    }
-
-    // public function searchTrack(Request $request, TrackRepository $track) {
-
-    //     $searchForm = $this->createForm(SearchType::class);
-    //     $searchForm->handleRequest($request);
-        
-    //     $data = $track->findAll();
- 
-    //     if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-    //         $title = $searchForm->getData()->getTitle();
-    //         $data = $track->search($title);
-
-    //         if ($data == null) {
-    //             $this->addFlash('erreur', 'Aucun morceau ne correspond dans la radio.');
-    //         }
-    //     }
-        
-    //     return $this->render('track/newtrack.html.twig', [
-    //         'searchForm' => $searchForm->createView(),
-    //         ]);
-    // }
-    
+        ]);   
+    }  
 
     /**
      * @Route("/tracks/{id}", name="app_track_show")
