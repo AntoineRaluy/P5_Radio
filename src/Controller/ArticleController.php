0e7826b79c0faf23792ee63a7e2262836939f70c
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2021-01-07 15:08:25 +0100

    Post comment function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use App\Repository\ArticleRepository;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
 use App\Service\MarkdownHelper;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
@@ -26,10 +31,32 @@ class ArticleController extends AbstractController
     /**
      * @Route("/articles/{slug}", name="app_article_show")
      */
-    public function show(Article $article)
-    {
+    public function show(Article $article, Request $request, EntityManagerInterface $entityManager)
+    {   
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $user= $this->get('security.token_storage')->getToken()->getUser();
+            $comment->setAuthor($user);
+            $comment->setArticle($article);
+            $comment->setCreatedAt(new \DateTime('now'));
+            
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('app_article_show', [
+                'slug' => $article->getSlug()
+            ]);
+        }
+        
         return $this->render('article/show.html.twig', [
+            'commentForm' => $form->createView(),
             'article' => $article,
+            'comment' => $comment,
         ]);
     }
 }
