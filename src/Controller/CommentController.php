c1729ca4dfe177816b33cffea399d1514b0150c3
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2021-01-13 09:32:08 +0100

    Redirect to the article after flagging comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Repository\ArticleRepository;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,13 +16,12 @@ class CommentController extends AbstractController
     /**
     * @Route("/articles/{slug}/flag{id}", name="app_flag_comment")
      */
-    public function flagComment(Comment $comment, Article $article, EntityManagerInterface $entityManager)
+    public function flagComment($slug, Comment $comment, ArticleRepository $articleRepository, EntityManagerInterface $entityManager)
     {      
-        dd($article);
         $comment->setIsFlagged(true);
-
         $entityManager->flush();
 
+        $article = $articleRepository->findOneBy(['slug' => $slug]);
         return $this->redirectToRoute('app_article_show', [
             'slug' => $article->getSlug(),
         ]);
