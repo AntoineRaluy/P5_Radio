41b02a2596c49939950f1b2967c849331f4bb939
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2021-01-16 01:39:04 +0100

    Add paginator method, policies and cookies

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,13 +5,11 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\Comment;
 use App\Form\CommentFormType;
-use App\Service\MarkdownHelper;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
@@ -19,9 +17,15 @@ class ArticleController extends AbstractController
      /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(ArticleRepository $repository)
+    public function index(ArticleRepository $repository, Request $request, PaginatorInterface $paginator)
     {
-        $articles = $repository->findAllPostedOrderedByNewest();
+        $data = $repository->findAllPostedOrderedByNewest();    
+
+        $articles = $paginator->paginate(           // 3 articles per page
+            $data,
+            $request->query->getInt('page', 1),
+            3
+        );
 
         return $this->render('article/homepage.html.twig', [
             'articles' => $articles,
@@ -31,7 +35,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/articles/{slug}", name="app_article_show")
      */
-    public function show(Article $article, Request $request, EntityManagerInterface $entityManager)
+    public function displayArticle(Article $article, Request $request, EntityManagerInterface $entityManager)
     {   
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -40,7 +44,7 @@ class ArticleController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $comment = $form->getData();
-            $user= $this->get('security.token_storage')->getToken()->getUser();
+            $user= $this->get('security.token_storage')->getToken()->getUser();     // get current user
             $comment->setAuthor($user);
             $comment->setArticle($article);
             $comment->setCreatedAt(new \DateTime('now'));
@@ -53,10 +57,26 @@ class ArticleController extends AbstractController
             ]);
         }
         
-        return $this->render('article/show.html.twig', [
+        return $this->render('article/article.html.twig', [
             'commentForm' => $form->createView(),
             'article' => $article,
             'comment' => $comment,
         ]);
     }
-}
+
+    /**
+     * @Route("/policies", name="app_policies")
+     */
+    public function policies()
+    {
+        return $this->render('policies.html.twig');
+    }
+
+    /**
+     * @Route("/cookies", name="app_cookies")
+     */
+    public function cookies()
+    {
+        return $this->render('cookies.html.twig');
+    }
+}   
