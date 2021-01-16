da57017f052025c67a01b0660d6f3fdca44e81cc
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2021-01-16 02:16:48 +0100

    Request new track via new form

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -4,13 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Track;
 use App\Form\TrackFormType;
-use App\Service\MarkdownHelper;
+use App\Form\NewTrackFormType;
 use App\Repository\TrackRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrackController extends AbstractController
@@ -35,29 +35,27 @@ class TrackController extends AbstractController
         $form = $this->createForm(TrackFormType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $track = $form->getData();
             $title = $track->getTitle();
             $artist = $track->getArtist();
-
+                                                    
             if (($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_USER')) == true) {
                 $user= $this->get('security.token_storage')->getToken()->getUser();
-                $track->setContributor($user);
-            }
-            
+                $track->setContributor($user);      // set contributor if member of the site
+            }            
 
             $resultTrack = $repoTrack->findTrack($artist, $title);
             
             if ($resultTrack == !null) {
-                $this->addFlash('erreur', 'Le morceau est déjà dans la radio');
+                $this->addFlash('erreur', 'Le morceau demandé est déjà dans la radio.');
                 return $this->redirectToRoute('app_tracklist');
                 }
-
             else {
-
                 $entityManager->persist($track);
                 $entityManager->flush();
-                $this->addFlash('info', 'Le morceau est maintenant en attente de validation');
+
+                $this->addFlash('info', 'Le morceau est maintenant en attente de validation.');
                 return $this->redirectToRoute('app_tracklist');
             }
         } 
@@ -76,4 +74,37 @@ class TrackController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/contact/req", name="app_request_track")
+     */
+    public function requestTrack(Request $request, MailerInterface $mailer)
+    {
+        $form = $this->createForm(NewTrackFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reqTrack = $form->getData();
+
+            $email = (new TemplatedEmail())
+                ->from($reqTrack['from'])
+                ->to('antoine.raluy@hotmail.fr')
+                ->subject('[RADIO Request New Track]')
+                ->htmlTemplate('email/reqtrackmail.html.twig')
+                ->context([
+                    'artist' => $reqTrack['artist'],
+                    'title' => $reqTrack['title'],
+                    'from' => $reqTrack['from'],
+                    'source' => $reqTrack['source']
+                ]);
+            $mailer->send($email);   
+            
+            $this->addFlash('info', 'Votre demande a bien été prise en compte.');
+
+            return $this->redirectToRoute('app_track_new');
+        }
+
+        return $this->render('track/requesttrack.html.twig', [
+            'newTrackForm' => $form->createView(),
+        ]);
+    }
 }
---
+++ src/Form/NewTrackFormType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class NewTrackFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('artist', TextareaType::class, [
+                'label' => 'Nom de l\'artiste :'
+            ])
+            ->add('title', TextareaType::class, [
+                'label' => 'Titre du morceau :'
+            ])
+            ->add('source', UrlType::class, [
+                'label' => 'Adresse du site avec des détails sur le morceau :',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir un site avec les informations sur le morceau.'
+                    ])
+                ],
+            ])
+            ->add('from', EmailType::class, [
+                'label' => 'Votre adresse mail :',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre adresse mail.'
+                    ])
+                ],
+            ]);   
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
