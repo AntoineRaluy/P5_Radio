cc67aeea569ac00201e3a2fd8b07c74fae29797e
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2021-01-16 02:17:39 +0100

    Deny access to profile to other users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,28 +5,39 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserFormType;
 use App\Service\FileUploader;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class UserController extends AbstractController
 {   
     /**
-    * @Route("/user", name="app_user")
+    * @Route("/profile/{username}", name="app_user")
     */
-    public function index() 
-    {
+    public function profile(User $user) 
+    {   
+        if ($user->getUsername() !== $this->getUser()->getUsername()) {     // prevents other users to acces the page
+            throw $this->createAccessDeniedException();
+        }
+        
         return $this->render('user/profile_view.html.twig'); 
     }
 
     /**
-     * @Route("/user/{username}/edit", name="app_user_edit")
+     * @Route("/profile/{username}/edit", name="app_user_edit")
     */
     public function editProfile(User $user, Request $request, FileUploader $fileUploader, EntityManagerInterface $entityManager)
-    {
+    {   
+        if ($user->getUsername() !== $this->getUser()->getUsername()) {     // prevents other users to acces the page
+            throw $this->createAccessDeniedException();
+        }
+
         $form = $this->createForm(UserFormType::class, $user);
         $form->handleRequest($request);
 
@@ -34,19 +45,18 @@ class UserController extends AbstractController
              /** @var UploadedFile $avatarFile */
              $avatarFile = $form->get('imageFilename')->getData();
             
-                if ($avatarFile) {
-                    $avatarFileName = $fileUploader->upload($avatarFile);
-                    $user->setImageFilename($avatarFileName);
-                    
-                }
-                
-                $entityManager->flush();
-
-                return $this->redirectToRoute('app_user');
+            if ($avatarFile) {
+                $avatarFileName = $fileUploader->upload($avatarFile);
+                $user->setImageFilename($avatarFileName);  
             }
+            $entityManager->flush();
+
+            $this->addFlash('info', 'Votre profil a été mis à jour.');
+            return $this->redirectToRoute('app_user');
+        }
     
-            return $this->render('user/profile_edit.html.twig', [
-                'userForm' => $form->createView(),
-            ]);
+        return $this->render('user/profile_edit.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
     }
 }
